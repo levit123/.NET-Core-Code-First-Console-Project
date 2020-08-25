1abc53806946b07b9cf3e1aa718fcfd4684ed7ef
levi <levicaseblodgett@gmail.com>
2020-08-25 13:52:58 -0700

    init commit

---
+++ EntityFramework_Student/EntityFramework_Student/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EntityFramework_Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // uses SchoolContext to create a default set of students to initially populate the database with
+            using (var ctx = new SchoolContext())
+            {
+                // creates and defines three Student objects
+                var stud1 = new Student() { StudentName = "Billy" };
+                var stud2 = new Student() { StudentName = "Sally" };
+                var stud3 = new Student() { StudentName = "Mandy" };
+
+                // adds the Student objects to the SchoolContext
+                ctx.Students.Add(stud1);
+                ctx.Students.Add(stud2);
+                ctx.Students.Add(stud3);
+
+                ctx.SaveChanges();
+
+                foreach (var stud in ctx.Students)
+                {
+                    Console.WriteLine("Student ID: " + stud.StudentID + " Student Name: " + stud.StudentName);
+                }
+            }
+        }
+    }
+}
---
+++ EntityFramework_Student/EntityFramework_Student/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace EntityFramework_Student
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(): base("MySchoolDB")
+        {
+            Database.SetInitializer<SchoolContext>(new CreateDatabaseIfNotExists<SchoolContext>());
+        }
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
